c045e8f7e16e587cbfcdb1cd4d3de8e7f359224c
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-07-18 18:59:04 +0200

    ex 5

--- 1_basics_pgg/ex_5.py
+++ 1_basics_pgg/ex_5.py
@@ -1,7 +1,13 @@
 """
 Write a program to check if the number given by the user:
-is odd, divisible by 3 and greater than 10
+is odd (number % 2 != 0), divisible by 3 (number % 3 == 0) and greater than 10 (number > 10)
 or
-is the number 7
+is the number 7 (number == 7)
 """
 
+number = int(input('Provide a number: '))
+
+# https://docs.python.org/3/reference/expressions.html#operator-precedence
+result = (number % 2 != 0 and number % 3 == 0 and number > 10) or number == 7
+
+print(f'Result: {result}')
