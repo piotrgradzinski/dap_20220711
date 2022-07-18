8699379649d41297d5edffc543e9beea50cc52e9
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-07-18 19:25:48 +0200

    ex 6

--- 1_basics_pgg/ex_6.py
+++ 1_basics_pgg/ex_6.py
@@ -5,3 +5,12 @@ Sample program output:
 Enter year of birth: 1980
 You are of legal age!
 """
+
+year_of_birth = int(input('Enter year of birth: '))
+
+age = 2022 - year_of_birth
+
+if age >= 18:
+    print('You are of legal age!')
+else:
+    print('You are not of legal age!')
