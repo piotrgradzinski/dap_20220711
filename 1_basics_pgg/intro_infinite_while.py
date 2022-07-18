02e40bee2584c6cb30f9c322599505afbb43a08b
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-07-18 20:29:55 +0200

    infinite loop

---
+++ 1_basics_pgg/intro_infinite_while.py
@@ -0,0 +1,9 @@
+while True:
+    name = input('Provide your name or end to stop: ')
+
+    if name == 'end':
+        break
+    else:
+        print(f'Hello {name}')
+
+print('The program has finished.')
