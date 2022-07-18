cdecb48c49d6a74db777080a1dac2bd129d931ca
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-07-18 19:50:55 +0200

    ex 7

--- 1_basics_pgg/ex_7.py
+++ 1_basics_pgg/ex_7.py
@@ -7,6 +7,27 @@ Sample program output:
 Enter the first number: 10
 Enter the second number: 5
 Enter the type of operation (+, -, *, /): +
-Result: 15
+Result: 10 + 5 = 15
 """
+
+number_1 = float(input('Enter the first number: '))
+number_2 = float(input('Enter the second number: '))
+operation = input('Enter the type of operation (+, -, *, /): ')
+
+if operation == '+':
+    result = number_1 + number_2
+elif operation == '-':
+    result = number_1 - number_2
+elif operation == '*':
+    result = number_1 * number_2
+elif operation == '/':
+    result = number_1 / number_2
+else:
+    result = None
+
+
+if result is None:
+    print('You have provided wrong operation!')
+else:
+    print(f'{number_1} {operation} {number_2} = {result}')
 
