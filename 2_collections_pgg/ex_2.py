0a184f488eff1a0b9c96580cc57442338845add8
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-07-20 19:35:27 +0200

    collections, ex 2.

--- 2_collections_pgg/ex_2.py
+++ 2_collections_pgg/ex_2.py
@@ -3,3 +3,12 @@ Write a program that calculates the average of numbers provided by the user.
 To store the numbers use list. Allow up to 10 numbers to be entered. Use the `sum()` built-in function.
 """
 
+numbers = []
+
+while len(numbers) < 10:
+    number = int(input('Provide a number: '))
+    numbers.append(number)
+
+average = sum(numbers) / len(numbers)
+
+print(f'The average from the provided numbers equals {average:.2f}')
