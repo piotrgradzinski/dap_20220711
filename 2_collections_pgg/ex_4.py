25556f6af887631745737f0dee379a1718a92707
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-07-20 20:13:08 +0200

    collections, ex 4.

--- 2_collections_pgg/ex_4.py
+++ 2_collections_pgg/ex_4.py
@@ -14,3 +14,12 @@ Numbers divisible by 3 or 5:
 In the range 0-100 there are 48 numbers divisible by 3 or 5
 """
 
+how_many = 0
+for i in range(0, 101):
+    if i % 3 == 0 or i % 5 == 0:
+        print(i)
+        how_many += 1
+
+print()
+print(how_many)
+
