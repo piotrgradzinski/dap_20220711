7168f719a056cc1f12890ab9c6bfd5c8ba28e814
Krzysztof Adamek <kadamek@alx.pl>
2022-07-27 20:32:59 +0200

    homework

---
+++ 2_collections_ka/ex_11.py
@@ -0,0 +1,20 @@
+# Write a program that counts the number of unique numbers entered
+# by the user. Check how many of these numbers are even numbers in
+# the range 0-100 - to do this use the intersection operator.
+# Use set()
+# Examlple:
+#
+# Data:
+# 1
+# 2
+# 3
+# 2
+# 3
+# 4
+# 5
+# 200
+# end
+#
+# Result:
+# "You've entered 6 unique numbers"
+# "2 of them are even numbers from the range 0-100"
---
+++ 2_collections_ka/ex_6.py
@@ -0,0 +1,4 @@
+# Write a program that, given list of numbers, swaps the largest
+# element with the smallest.
+#
+# [4, 10, 5, 1, 7] should turn into [4, 1, 5, 10, 7]
